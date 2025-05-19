e02a1596bff5b8b0bfcb8e05939653bf02cb4b84
Dhruv-garg17 <dhruv1689.be21@chitkara.edu.in>
2025-05-19 23:24:41 +0530

    port update

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://skai-lama-backend-zk0v.onrender.com';
+const API_URL = 'https://skai-lama-backend-zk0v.onrender.com/api';
 // const API_URL = 'http://localhost:5000/api';
 
 // User services
